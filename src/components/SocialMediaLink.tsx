3911723554d73eca25a2f2ca28d87ded5ef7f189
Marcos Almorox <marcos.almorox@gmail.com>
2024-01-25 20:03:41 +0100

    Cambios varios antes de salir

---
+++ src/components/SocialMediaLink.tsx
@@ -0,0 +1,27 @@
+import type { NavItem } from "../types/interfaces.ts";
+const socialMediaLinks: NavItem[] = [
+    {
+        title: "Home",
+        url: "/",
+    },
+    {
+        title: "Orders",
+        url: "/orders",
+    },
+    {
+        title: "About Us",
+        url: "/about-us",
+    },
+    {
+        title: "Contact",
+        url: "/contact",
+    },
+];
+
+export function SocialMediaLink() {
+    return (
+        <a href={}>
+
+        </a>
+    );
+}
