913ca595cff40bf7a2adc467c195bb519838fff5
Marcos Almorox <marcos.almorox@gmail.com>
2024-02-02 21:01:28 +0100

    Cambios antes de salir

--- src/data/info.ts
+++ src/data/info.ts
@@ -3,10 +3,9 @@ import type { Presentation } from '@/utils/types/presentation';
 const presentation: Presentation = {
     mail: 'marcosalmorox@gmail.com',
     title: '*Hi*, I’m Marcos',
-    profile: '/profile-logo.webp',
+    profile: '/',
     description:
-        "**IT** System Technician actively transitioning towards a more **coding**-focused career. I am dedicated to honing my expertise in **B**ackend using **Python** and expanding my capabilities in **F**rontend with **NextJS** and **Astro**. Fascinated by **AI** and its potential, exploring **Machine Learning** and **LLM**'s.",
-
+        '**IT** System Technician actively transitioning towards a more **coding**-focused career. I am dedicated to honing my expertise in **B**ackend using **Python** and expanding my capabilities in **F**rontend with **NextJS** and **Astro**. Fascinated by **AI** and its potential, exploring **Machine Learning** and **LLM**',
     socials: [
         {
             label: 'GitHub',
