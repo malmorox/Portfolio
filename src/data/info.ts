21146c85a2e9c0e14cb86751240d88ba2e2916a6
Marcos Almorox <marcos.almorox@gmail.com>
2024-02-06 15:40:57 +0100

    Cambios pequeños

--- src/data/info.ts
+++ src/data/info.ts
@@ -5,7 +5,7 @@ const presentation: Presentation = {
     title: '*Hi*, I’m Marcos',
     profile: '/',
     description:
-        '**IT** System Technician actively transitioning towards a more **coding**-focused career. I am dedicated to honing my expertise in **B**ackend using **Python** and expanding my capabilities in **F**rontend with **NextJS** and **Astro**. Fascinated by **AI** and its potential, exploring **Machine Learning** and **LLM**',
+        '',
     socials: [
         {
             label: 'GitHub',
