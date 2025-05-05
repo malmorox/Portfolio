9dd76145e2a871ea92a1ea200d9c75f718565920
Marcos Almorox <marcos.almorox@gmail.com>
2025-05-05 18:21:59 +0200

    cambios antes de desplegarlo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
-import i18next from 'astro-i18next';
+//import i18next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(),
+  integrations: [tailwind(), react()/*,
     i18next({
       defaultLocale: 'es',
       locales: ['es', 'en'],
       loadLocaleFrom: './src/locales'
 
-    })
+    })*/
   ]
 });
